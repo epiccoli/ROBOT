78cf8e139f4557bf465e6d00b8e1757373c05c7f
Christopher Tchervenkov <christophertchervenkov@tsf-428-wpa-7-219.epfl.ch>
2015-06-07 14:26:46 +0200

    added Arms lib

---
+++ Chips/libraries/Arms/Arms.cpp
@@ -0,0 +1,95 @@
+/* Define a class object for the arms.
+
+
+
+*/
+
+// Destructor maybe not needed for arms, but for door only (arms are always functioning)
+
+#include "Arms.h"
+
+Arms::Arms(){
+	_ID_leftArm = ID_ARM_LEFT;
+	_foldPos_leftArm = FOLD_POS_LEFT;
+	_openPos_leftArm = OPEN_POS_LEFT;
+	_grabPos_leftArm = GRAB_POS_LEFT;
+
+	_ID_rightArm = ID_ARM_RIGHT;
+	_foldPos_rightArm = FOLD_POS_RIGHT;
+	_openPos_rightArm = OPEN_POS_RIGHT;
+	_grabPos_rightArm = GRAB_POS_RIGHT;
+	
+	_vel = 80;
+
+	// Dynamixel setup
+	Dynamixel.begin(1000000,2);
+	Dynamixel.setEndless(_ID_leftArm, OFF);
+    Dynamixel.setEndless(_ID_rightArm, OFF);
+}
+
+
+
+bool Arms::fold(){
+
+	  Dynamixel.moveSpeed(_ID_leftArm,_foldPos_leftArm,_vel);
+      Dynamixel.moveSpeed(_ID_rightArm,_foldPos_rightArm,_vel);
+
+      return true;  //TODO: think of stop condition to return false
+
+}
+
+
+bool Arms::open(){
+
+	Dynamixel.moveSpeed(_ID_leftArm,_openPos_leftArm,_vel);
+    Dynamixel.moveSpeed(_ID_rightArm,_openPos_rightArm,_vel);
+
+    return true;
+}
+
+
+bool Arms::grab(){
+
+    Dynamixel.moveSpeed(_ID_leftArm,_grabPos_leftArm,_vel);
+    Dynamixel.moveSpeed(_ID_rightArm,_grabPos_rightArm,_vel); 
+    
+//    int LeftPos = Dynamixel.readPosition(_ID_leftArm);
+//    int RightPos = Dynamixel.readPosition(_ID_rightArm);
+//    
+//    Serial.print("LeftPos: ");
+//    Serial.println(LeftPos);
+//    Serial.print("RightPos: ");
+//    Serial.println(RightPos);
+//    return true;
+}
+
+bool Arms::moveArm() {
+       
+        int choice = 0;
+	Serial.println("LEFT: (<750!!)");
+	Serial.print("> "); //curseur
+	while(Serial.available() == 0){ //wait for user to enter key
+	}
+	choice = Serial.parseInt(); // reads choice
+	Serial.println(choice); //print choice
+	Serial.print("\n"); //space line
+	delay(200);
+
+	Dynamixel.moveSpeed(_ID_leftArm,choice,_vel);
+
+	Serial.println("RIGHT: (<800!!)");
+	Serial.print("> "); //curseur
+	while(Serial.available() == 0){ //wait for user to enter key
+	}
+	choice = Serial.parseInt(); // reads choice
+	Serial.println(choice); //print choice
+	Serial.print("\n"); //space line
+	delay(200);
+
+	Dynamixel.moveSpeed(_ID_rightArm,choice,_vel); 
+        
+
+	return true;
+
+}
+ 
---
+++ Chips/libraries/Arms/Arms.h
@@ -0,0 +1,65 @@
+/* Define a class object for the arms.
+
+need to do: 
+
+calibrate the dynamixel
+
+
+*/
+
+
+#ifndef Arms_INCLUDED
+#define Arms_INCLUDED
+
+#include <Arduino.h>
+#include "DynamixelSerial.h"
+
+
+#define ID_ARM_LEFT 7
+
+#define OPEN_POS_LEFT 850
+#define FOLD_POS_LEFT 550
+#define GRAB_POS_LEFT 300
+#define MAX_LEFT 750
+
+#define ID_ARM_RIGHT 1 // it is written 6
+
+#define OPEN_POS_RIGHT 300
+#define FOLD_POS_RIGHT 570
+#define GRAB_POS_RIGHT 820
+#define MAX_RIGHT 930
+
+class Arms
+{
+public:
+	Arms(); 
+	/* when the constructor is activated, it does not compile giving "In function `setup':
+/Users/emilepiccoli/Test_Grab_arduino.ino:11: undefined reference to `Arms::~Arms()' 
+collect2: error: ld returned 1 exit status Error compiling. */
+
+    // ~Arms();
+
+	// Functions needed: bool only to return if all is OK
+	bool fold();
+	bool open();
+	bool grab();
+	bool moveArm();
+
+
+
+private: 
+	int _ID_leftArm;                // ID of dynamixel servo
+	int _foldPos_leftArm;		// position for folding arms 
+	int _openPos_leftArm;		// position for opening (to set in Dynamixel.moveSpeed(ID,pos,speed))		
+	int _grabPos_leftArm;		// *
+
+    int _ID_rightArm;	        // ID of dynamixel servo
+    int _foldPos_rightArm;		// position for folding arms 
+	int _openPos_rightArm;		// position for opening (to set in Dynamixel.moveSpeed(ID,pos,speed))		
+	int _grabPos_rightArm;		// *
+
+	int _vel;			// set the dynamixel speed 0-1023
+	//int _velGrab;		// set the dynamixel speed for grabbing --> where do I put the value if it should stay constant? I put it in the cpp
+}; 
+
+#endif // grab_H_INCLUDED
